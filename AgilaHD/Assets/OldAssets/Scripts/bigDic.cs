cd809c69443f609da1c957f2152b480c8644970e
jancarlosanjuan <58461098+jancarlosanjuan@users.noreply.github.com>
2021-06-03 06:15:01 +0800

    added glide and tilting at fall

--- AgilaHD/Assets/OldAssets/Scripts/bigDic.cs
+++ AgilaHD/Assets/OldAssets/Scripts/bigDic.cs
@@ -28,11 +28,16 @@ public class bigDic : MonoBehaviour
     private float currRot = 0;
     private float smoothVel;
 
+    Vector3 lastVelocity;
+    private float initialDrag;
+    private float initialAngularDrag;
+    Vector3 lastPosition;
     // Start is called before the first frame update
     void Start()
     {
         //cameraT = Camera.main.transform;
-        
+        initialDrag = gameObject.GetComponent<Rigidbody>().drag;
+        initialAngularDrag = gameObject.GetComponent<Rigidbody>().angularDrag;
     }
 
     // Update is called once per frame
@@ -52,9 +57,9 @@ public class bigDic : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            
 
-            
+            gameObject.GetComponent<Rigidbody>().drag = initialDrag;
+            gameObject.GetComponent<Rigidbody>().angularDrag = 0.3f;
             yawRotation = tpsReference.GetComponent<ThirdPersonCamera>().yawRotation;
 
             if (tpsReference.GetComponent<ThirdPersonCamera>().moveleft)
@@ -74,9 +79,9 @@ public class bigDic : MonoBehaviour
 
             Quaternion too;
             too = Quaternion.Euler(1, 1, -currRot);
-            
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, cameraT.rotation * too, Time.deltaTime / rotationSmoothTime);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, cameraT.rotation * too, Time.deltaTime / rotationSmoothTime) ;
             
             
             rb.AddForce(ForwardForce * transform.forward);
@@ -85,11 +90,48 @@ public class bigDic : MonoBehaviour
         }
         else
         {
+            Vector3 velocity = gameObject.GetComponent<Rigidbody>().velocity;
+            if (gameObject.transform.position.y <= lastPosition.y)//means we're going down
+            {
+                Debug.Log("going down");
+                rb.AddForce(7.0f * (transform.forward + Vector3.up)); //glide
+            }
+            else
+            {
+                Debug.Log("going up");
+                rb.AddForce(2.0f * (transform.forward + Vector3.up)); //glide
+            }
             
-            transform.position += transform.forward * (IdleForce) * Time.deltaTime;
+
+            gameObject.GetComponent<Rigidbody>().angularDrag = initialAngularDrag;
+            float nice = gameObject.GetComponent<Rigidbody>().drag;
+            if(nice > 0.15f)
+            {
+                gameObject.GetComponent<Rigidbody>().drag = nice - Time.deltaTime;
+            }
+            
+            //transform.position += transform.forward * (IdleForce) * Time.deltaTime;
+
+            //look forward based on velocity
+            //Vector3 velocity= gameObject.GetComponent<Rigidbody>().velocity;
+            float magnitude = velocity.magnitude;
+            
+
+            Vector3 orignialDirection = this.lastVelocity;
+            Vector3 nowDirection = gameObject.GetComponent<Rigidbody>().velocity;
+            Vector3 rotationAxis = (Vector3.Cross(orignialDirection, nowDirection));
+            float angle = (Vector3.Angle(orignialDirection, gameObject.GetComponent<Rigidbody>().velocity)) * Mathf.Deg2Rad / 2;
+            Quaternion q = Quaternion.FromToRotation(orignialDirection, nowDirection);
+
+
+            //Quaternion quat = Quaternion.Euler(interpTo);
+            //Debug.Log(magnitude);
+            transform.rotation = Quaternion.RotateTowards(gameObject.transform.rotation, q * transform.rotation, magnitude / 10) ;
+
+            //Quaternion newFace = Quaternion.LookRotation(velocity);
         }
 
-
-
+        this.lastPosition = gameObject.transform.position;
+        this.lastVelocity = gameObject.GetComponent<Rigidbody>().velocity;
     }
 }
