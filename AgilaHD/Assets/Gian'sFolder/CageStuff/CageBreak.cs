647063bb52000280ab90832d1dfcec6fb9985ea1
Gian Mauricio <gian_mauricio@dlsu.edu.ph>
2021-08-03 13:33:31 +0800

    Cage Materials fixed

--- AgilaHD/Assets/Gian'sFolder/CageStuff/CageBreak.cs
+++ AgilaHD/Assets/Gian'sFolder/CageStuff/CageBreak.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class CageBreak : MonoBehaviour
 {
     public GameObject BrokenCage;
+    public float timer = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            Break();
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Bird"))
         {
             Break();
         }
